a82103eea738509bb532c08d9e0d58e069ea5845
Evgeniy Kamskiy <kamskiie@gmail.com>
2022-11-27 04:55:29 +0300

    Add homework5 fixes

--- tests/test_element_visibility.py
+++ tests/test_element_visibility.py
@@ -4,53 +4,60 @@ from selenium.webdriver.support import expected_conditions as EC
 
 CATALOG_URL_PATH = "/component/monitor"
 CARD_PRODUCT_URL_PATH = "/component/monitor/samsung-syncmaster-941bw"
-LOGIN_PAGE_URL_PATH = "/index.php?route=account/login"
+LOGIN_ADMIN_PAGE_URL_PATH = "/admin"
 REGISTER_PAGE_URL_PATH = "/index.php?route=account/register"
 
 
 def test_main_page(driver):
     driver.get(driver.base_url)
-    wait = WebDriverWait(driver=driver, timeout=1)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#logo")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#menu")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#search")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#top-links")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#cart")))
+    wait = WebDriverWait(driver=driver, timeout=5)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#slideshow0")))
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".slideshow div.swiper-button-next")))
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".slideshow div.swiper-button-prev")))
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div[class*='swiper-pagination slideshow0'] span")))
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#content .row .product-layout")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div[id = carousel0]")))
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div[class*='swiper-pagination carousel0'] span")))
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".carousel div.swiper-button-next")))
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".carousel div.swiper-button-prev")))
 
 
 def test_catalog(driver):
     driver.get(driver.base_url + CATALOG_URL_PATH)
-    wait = WebDriverWait(driver=driver, timeout=1)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#column-left")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#content")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[class='col-md-2 col-sm-6 hidden-xs']")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[class='breadcrumb']")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[class='col-sm-6 text-right']")))
+    wait = WebDriverWait(driver=driver, timeout=5)
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#column-left .list-group-item")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#list-view")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#grid-view")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-sort")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-limit")))
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#content .product-layout")))
 
 
 def test_card_product(driver):
     driver.get(driver.base_url + CARD_PRODUCT_URL_PATH)
-    wait = WebDriverWait(driver=driver, timeout=1)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[class='thumbnail']")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[class='nav nav-tabs']")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[class='tab-content']")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#button-cart")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[class='rating']")))
+    wait = WebDriverWait(driver=driver, timeout=5)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a[class='thumbnail']")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "ul[class$='nav-tabs']")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button[data-original-title*='Add to Wish List']")))
+    wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "button[data-original-title*='Compare this Product']")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-quantity")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button[id='button-cart']")))
 
 
-def test_login_page(driver):
-    driver.get(driver.base_url + LOGIN_PAGE_URL_PATH)
-    wait = WebDriverWait(driver=driver, timeout=1)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-email")))
+def test_login_admin_page(driver):
+    driver.get(driver.base_url + LOGIN_ADMIN_PAGE_URL_PATH)
+    wait = WebDriverWait(driver=driver, timeout=5)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-username")))
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-password")))
-    wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Forgotten Password")))
-    wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Continue")))
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[value='Login']")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "span[class='help-block'] a")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button[class$='btn-primary']")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#footer a")))
 
 
 def test_register_page(driver):
     driver.get(driver.base_url + REGISTER_PAGE_URL_PATH)
-    wait = WebDriverWait(driver=driver, timeout=1)
+    wait = WebDriverWait(driver=driver, timeout=5)
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-firstname")))
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-lastname")))
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#input-email")))
